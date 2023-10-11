e1103f76f4274606fd0ebc00c3abfd7408891282
Janice Chen <janicekchen@gmail.com>
2023-10-11 13:25:04 -0400

    update clam lights script

---
+++ code/clam-pixels.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import board
+import neopixel
+
+# defining colors
+green = (0, 255, 127)
+red = (255, 0, 0)
+orange = (255, 133, 0)
+
+# initiating lights with the correct pin (GPIO12) and number of lights
+pixels = neopixel.NeoPixel(board.D12, 18, brightness = 0.3)
+
+# reading in data
+beach_data = pd.read_csv("some CSV link")
+beach_data = pd.DataFrame(beach_data) # do i need this line?
+
+
+for i in range(0, 14):
+    # select value of led_status for a row
+    led_status = beach_data.loc[beach_data["beach_num"] == i, 'LED_status']
+
+    # if value is 0 -> light up pixel as red, etc. 
+    if led_status == 0:
+        pixels[i] = red
+    elif led_status == 1:
+        pixels[i] = orange
+    else:
+        pixels[i] = green
+
