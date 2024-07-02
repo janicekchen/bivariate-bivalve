bb8510cb78a1846234aea4fcc33587b8da254860
Janice Chen <janicekchen@gmail.com>
2024-07-01 17:13:56 -0700

    update led code

--- code/clam-pixels.py
+++ code/clam-pixels.py
@@ -11,14 +11,14 @@ orange = (255, 133, 0)
 pixels = neopixel.NeoPixel(board.D12, 18, brightness = 0.3)
 
 # reading in data
-beach_data = pd.read_csv("some CSV link")
-# convert to integer
-# do some downloading here 
-# bring in the request library
+beach_data = pd.read_csv("https://raw.githubusercontent.com/janicekchen/bivariate-bivalve/main/data/processed/bivariatebivalve-ledstatus.csv")
 
 for i in range(0, 14):
     # select value of led_status for a row
-    led_status = beach_data.loc[beach_data["beach_num"] == i, 'LED_status']
+    beach = beach_data.loc[beach_data["beach_num"] == i, 'BEACHNAME'].iloc[0]
+    led_status = beach_data.loc[beach_data["beach_num"] == i, 'LED_status'].iloc[0]
+
+    print(beach, led_status)
 
     # if value is 0 -> light up pixel as red, etc. 
     if led_status == 0:
@@ -28,6 +28,7 @@ for i in range(0, 14):
     else:
         pixels[i] = green
 
+# setting legend lights
 pixels[15] = red
 pixels[16] = orange
 pixels[17] = green
